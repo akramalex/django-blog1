d3091aeb0262540aba423ec4f767cd54f2e5a438
Akram Ewida <info@quick4you.net>
2024-10-01 15:14:58 +0000

    change new details on key

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g1er#8ywdy-apngsrx6)u85z*q70s8$4*_v)p6a)#q9k7w%@l$'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
--- env.py
+++ env.py
@@ -2,3 +2,5 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgres://u9jofjis4rg:6S4Lp7xzBZhG@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/five_snuff_audio_301679")
+
+os.environ.setdefault("SECRET_KEY", "akramewida")
