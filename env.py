4539aa5b611cc7acbb469c884e3bff291a38b060
Akram Ewida <info@quick4you.net>
2024-10-03 16:01:18 +0000

    issued fixed

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 import os
 
 os.environ.setdefault(
-    "DATABASE_URL", "postgres://u9jofjis4rg:6S4Lp7xzBZhG@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/five_snuff_audio_301679")
+    "DATABASE_URL", "postgres://u9jofjis4rg:6S4Lp7xzBZhG@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/gig_food_puma_800213")
 
 os.environ.setdefault("SECRET_KEY", "akramewida")
