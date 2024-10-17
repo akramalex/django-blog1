01e24f5a063c28168d23ee5569df20de720bbd8d
Akram Ewida <info@quick4you.net>
2024-10-17 11:46:00 +0000

    add message and link to homepage

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CollaborateRequest
 
-# Register your models here.
+# Register the CollaborateRequest model
+@admin.register(CollaborateRequest)
+class CollaborateRequestAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email','message', 'is_read', 'created_on')
+    list_filter = ('is_read',)
+    
--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .forms import CollaborateRequestForm
 
 def collaborate_view(request):
@@ -6,6 +7,7 @@ def collaborate_view(request):
         form = CollaborateRequestForm(request.POST)
         if form.is_valid():
             form.save()  # Save the form data to the database
+            messages.success(request, "Collaboration request received! I endeavor to respond within 2 working days.")
             return redirect('collaborate_success')  # Redirect to success page
     else:
         form = CollaborateRequestForm()  # Create an empty form
