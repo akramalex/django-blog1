1fd17908be3cc102a1f53b5a7b6fa492020aa58e
Akram Ewida Ahmed Ewida <info@quick4you.net>
2024-10-18 13:17:02 +0000

    update to false

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-akramalex-djangoblog1-162wwx52238.ws.codeinstitute-ide.net','.herokuapp.com']
 
