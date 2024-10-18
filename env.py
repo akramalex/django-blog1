8365949fd78361c052cf3286b05bab488c0dacff
Akram Ewida Ahmed Ewida <info@quick4you.net>
2024-10-18 13:14:45 +0000

    enable serving of image files

--- env.py
+++ env.py
@@ -4,3 +4,6 @@ os.environ.setdefault(
     "DATABASE_URL", "postgres://u9jofjis4rg:6S4Lp7xzBZhG@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/gig_food_puma_800213")
 
 os.environ.setdefault("SECRET_KEY", "akramewida")
+
+os.environ.setdefault(
+    "CLOUDINARY_URL", "cloudinary://749463183397936:_OoG0oh7zXDOzpjPKxSr7Cq06rA@dni9srymh")
